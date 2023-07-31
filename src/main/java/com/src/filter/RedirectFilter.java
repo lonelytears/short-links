36b3563d8a16dd9c14d7761b440bfc430e9db1d5
钟思名 <c-zhongsm01@vanke.com>
2023-07-31 10:42:29 +0800

    新增swagger依赖

--- src/main/java/com/src/filter/RedirectFilter.java
+++ src/main/java/com/src/filter/RedirectFilter.java
@@ -23,35 +23,35 @@ public class RedirectFilter  implements Filter {
     @SneakyThrows
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        log.info("doFilter start");
-        if (servletRequest instanceof HttpServletRequest) {
-
-            String url = ((HttpServletRequest)servletRequest).getRequestURL().toString();
-
-            String queryString = ((HttpServletRequest)servletRequest).getQueryString();
-            log.info("url:{} queryString:{}", url, queryString);
-
-            String[] str = url.split("/");
-            log.info("len:{}", str.length);
-            log.info("len:{}", str[str.length-1]);
-            // 处理短链接进来的数据
-            if (str.length == 4 && queryString == null) {
-                String shortCode = str[str.length-1];
-                QueryWrapper queryWrapper = new QueryWrapper();
-                queryWrapper.eq("short_link", shortCode);
-                ShortLinks links =  shortLinksMapper.selectOne(queryWrapper);
-                if (links == null) {
-                    throw new Exception("链接异常，请再试一次！");
-                }
-                log.info("重定向 url:{}", links.getOriginalLink());
-                HttpServletResponse resp = (HttpServletResponse) servletResponse;
-                resp.sendRedirect(links.getOriginalLink());
-                log.info("重定向 end");
-            } else {
-                filterChain.doFilter(servletRequest,servletResponse);
-            }
-        }
-
+        //log.info("doFilter start");
+        //if (servletRequest instanceof HttpServletRequest) {
+        //
+        //    String url = ((HttpServletRequest)servletRequest).getRequestURL().toString();
+        //
+        //    String queryString = ((HttpServletRequest)servletRequest).getQueryString();
+        //    log.info("url:{} queryString:{}", url, queryString);
+        //
+        //    String[] str = url.split("/");
+        //    log.info("len:{}", str.length);
+        //    log.info("len:{}", str[str.length-1]);
+        //    // 处理短链接进来的数据
+        //    if (str.length == 4 && queryString == null) {
+        //        String shortCode = str[str.length-1];
+        //        QueryWrapper queryWrapper = new QueryWrapper();
+        //        queryWrapper.eq("short_link", shortCode);
+        //        ShortLinks links =  shortLinksMapper.selectOne(queryWrapper);
+        //        if (links == null) {
+        //            throw new Exception("链接异常，请再试一次！");
+        //        }
+        //        log.info("重定向 url:{}", links.getOriginalLink());
+        //        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        //        resp.sendRedirect(links.getOriginalLink());
+        //        log.info("重定向 end");
+        //    } else {
+        //        filterChain.doFilter(servletRequest,servletResponse);
+        //    }
+        //}
+        filterChain.doFilter(servletRequest,servletResponse);
 
     }
 }
